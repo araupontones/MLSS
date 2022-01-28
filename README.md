bd20eff871ad1d49118624af07effae2d428efd8
araupontones <43657130+araupontones@users.noreply.github.com>
2022-01-28 17:39:58 +0100

    set up uploading of data

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # MLSS
 Dashboard of the MLSS survey
+
+# Importing zip files from rounds
+A zip file with the derived datasets of students, teachers, and schools.
+
+[Uploads and downloads](https://mastering-shiny.org/action-transfer.html)
+
+
+## Guide to Importing
+
+* Create a zip file with the three datasets. The format of the datasets must be .dta
+* The names of the .dta files should contain the words school, teacher, and students.
+
+
+### Importing backend protocol
+
+1. Check that the selected round has not been imported yet (if imported, error, import)
+2. Check that the files contain three files and that they are named school, teacher, student
+3. Check that the files contain the variables indicated in the guide
+4. Save in data/imports/round
+
+
+## things to review
+
+a) error messages
+b) stop if error
+c) reactivity of datasets
---
+++ functions/unzip_import.R
@@ -0,0 +1,56 @@
+#' function to import the data. Will take the zip file submited by the user and
+#' will unzip it into data/imports/round
+#' @param zipDir Directory of the zipped file 
+#' @param round round of the data
+#' @param importDir directory where data will be unzipped
+
+
+
+unzip_import <- function(zipDir
+                         ,round,
+                         unzipDir){
+  
+  levels <- c("school", "teacher", "student")
+  #unzip into temp directory
+  unzip(zipDir, overwrite = T, exdir = tempdir())
+  #move from temp to imports -------------------------------------------------
+  
+  #list of zipped files in stata format
+  temps_stata_files <- list.files(tempdir(), recursive = T, pattern = ".dta", full.names = T)
+  
+ 
+  #create directory for this round (this should be checked before running this protocol)
+  exDir <- file.path(unzipDir, round)
+  
+  if(!exists(exDir)){
+    
+    dir.create(exDir)
+  }
+  
+  for(stata_file in temps_stata_files){
+    
+    #to assure that names are in lower case
+    name_check <- str_to_lower(str_extract(stata_file, '[^\\/]+$'))
+    
+    #copy and rename the file of each level (school, teacher, student)
+    for(level in levels){
+      
+      if(str_detect(name_check, level)) {
+        
+        file.copy(stata_file, to = glue("{exDir}/{level}.dta"))
+      }
+      
+      
+      
+      
+      
+    }
+  }
+  
+}
+
+
+
+
+
+
