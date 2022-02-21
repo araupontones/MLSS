7e4a4e96dece4c88968e6694f1f7b6390854970a
araupontones <43657130+araupontones@users.noreply.github.com>
2022-02-21 14:54:00 +0100

    reduce size of images

---
+++ dashboard/R/styles.R
@@ -0,0 +1,28 @@
+color_bg <- "white"
+  #"#F2F3F6"
+
+theme_MLSS <- function(){
+  
+  theme(text = element_text(family = "Noto Sans"),
+        #legend
+        legend.position = 'top',
+        legend.background = element_rect(fill = color_bg),
+        legend.title = element_blank(),
+        #background
+        panel.background = element_rect(fill = color_bg),
+        plot.background = element_rect(fill = color_bg),
+        #grids
+        panel.grid = element_blank(),
+        panel.grid.major.y = element_line(linetype = "dotted", colour = "black"),
+        #axis
+        axis.ticks = element_blank(),
+        axis.title.y = element_text(margin = margin(r=20)),
+        axis.title.x = element_text(margin = margin(t=20)),
+        #strip
+        strip.text = element_text(hjust = 0, color = 'white'),
+        strip.background = element_rect(fill = "black")
+        
+        )
+}
+
+
